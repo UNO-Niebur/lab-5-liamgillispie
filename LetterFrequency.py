aa5a71968364e7377b80dbbb568a34d3698ea475
liamgillispie <l.gillispie526@gmail.com>
2026-02-22 20:26:21 +0000

    test

--- LetterFrequency.py
+++ LetterFrequency.py
@@ -1,34 +1,31 @@
 #LetterFrequency.py
-#Name:
-#Date:
-#Assignment:
+#Name:Liam Gillispie
+#Date:2/22/2026
+#Assignment:Lab 5
+#Purpose:Analyze how often a letter appears in a message
 
 #This program will create a CSV file of frequencies based on a text file.
 #Use Excel or similar spreadsheet software to visualize the frequencies of the CSV file.
 
 import os
 
-def countLetters(message):
+def countLetters(msg):
     alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    message = message.upper()
+    message = msg.upper()
 
     freq = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-    #loop through each letter
-    #Find the position in the alphabet
-    #Increase the frequency at that position. If position was 5, then frequencies[5] = frequencies[5] + 1
-
-
-
-    #Create the output text in the format A,5\n if there were 5 letter A in the message.
-    #Remember that the \n is the symbol for a new line.
-
+    for char in msg:
+        if 'a'<=char.lower()<='z':
+            position=ord(char.lower())-ord('a')
+            freq[position]+=1
+#reworked given function to fit the code written
     output = ""
-    for i in range(26):
-        print (alpha[i], ":", freq[i])
-        line = alpha[i] + "," + str(freq[i]) + "\n"
-        output = output + line
-
+    for index, count in enumerate(freq):
+        if count>=0:
+            letter=chr(ord('A')+index)
+            output+=f"{letter},{count}\n"
+    return output        
     writeToFile(output)
 
 
@@ -44,8 +41,15 @@ def writeToFile(fileText):
 
 def main():
     msg = input("Enter a message: ")
-    countLetters(msg)
+    result=countLetters(msg)
+    print(result)
+#loop through each letter
+    #Find the position in the alphabet
+    #Increase the frequency at that position. If position was 5, then frequencies[5] = frequencies[5] + 1
+        
 
+    #Create the output text in the format A,5\n if there were 5 letter A in the message.
+    #Remember that the \n is the symbol for a new line.
 
 
 if __name__ == '__main__':
